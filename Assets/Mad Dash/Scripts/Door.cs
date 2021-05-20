73cdced7986efa6ef58963d97660c1a43577026f
John <68535837+John-D-S@users.noreply.github.com>
2021-05-20 23:28:14 +1000

    Finished door scripts and implemented them

--- Assets/Mad Dash/Scripts/Door.cs
+++ Assets/Mad Dash/Scripts/Door.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField]
+    private GameObject leftDoorFrame;
+    [SerializeField]
+    private GameObject rightDoorFrame;
+    [SerializeField]
     private GameObject doorPart;
+
     //the localPosition.y of the door at its lowest
     private float closedDoorHeight;
     //the localPosition.y of the door at its highest
     [SerializeField]
     private float openedDoorHeight = 10;
+    //the width of the door
+    [SerializeField]
+    private float doorWidth = 10;
     [SerializeField]
     private float doorMoveSpeed = 3;
     private float DoorYpos => doorPart.transform.localPosition.y;
 
-    private MeshRenderer GateMeshRenderer;
+    private MeshRenderer leftDoorFrameMeshRenderer;
+    private MeshRenderer rightDoorFrameMeshRenderer;
 
     [System.NonSerialized]
     public bool open = false;
 
     //gets the target height of the door based on whether or not the bool open is true
-    float TargetHeight
+    private float TargetHeight
     {
         get
         {
@@ -33,14 +42,36 @@ public class Door : MonoBehaviour
         set { }
     }
 
+    private void OnValidate()
+    {
+        //setting the position of the left and right doorframes as specified in the inspector
+        Vector3 leftDoorFramePosition = Vector3.left * doorWidth * 0.5f + Vector3.up * openedDoorHeight * 0.5f;
+        leftDoorFrame.transform.localPosition = new Vector3(-doorWidth * 0.5f, openedDoorHeight * 0.5f, 0);
+        rightDoorFrame.transform.localPosition = new Vector3(doorWidth * 0.5f, openedDoorHeight * 0.5f, 0);
+        
+        //setting the scale of the doorframes to fit the height of the opened door;
+        Vector3 doorFrameScale = new Vector3(1, openedDoorHeight, 1);
+        leftDoorFrame.transform.localScale = doorFrameScale;
+        rightDoorFrame.transform.localScale = doorFrameScale;
+
+        doorPart.transform.localPosition = new Vector3(0, openedDoorHeight * 0.5f);
+        doorPart.transform.localScale = new Vector3(1 * doorWidth - 1, openedDoorHeight, 1);
+    }
+
     private void SetDoorHeight(float _YPos) => doorPart.transform.localPosition = new Vector3(doorPart.transform.localPosition.x, _YPos, doorPart.transform.localPosition.z);
 
-    public void SetColor(Color _color) => GateMeshRenderer.material.color = _color;
+    public void SetColor(Color _color) 
+    { 
+        leftDoorFrameMeshRenderer.material.color = _color;
+        rightDoorFrameMeshRenderer.material.color = _color;
+    }
 
     private void Start()
     {
         closedDoorHeight = DoorYpos;
-        GateMeshRenderer = GetComponent<MeshRenderer>();
+        leftDoorFrameMeshRenderer = leftDoorFrame.GetComponent<MeshRenderer>();
+        rightDoorFrameMeshRenderer = rightDoorFrame.GetComponent<MeshRenderer>();
+        SetColor(Color.red);
     }
 
     void Update()
@@ -51,6 +82,7 @@ public class Door : MonoBehaviour
             doorPart.transform.localPosition += Vector3.up * Time.deltaTime * doorMoveSpeed;
             if (DoorYpos > TargetHeight)
                 SetDoorHeight(TargetHeight);
+                
         }
         else if (DoorYpos > TargetHeight)
         {
--- Assets/Mad Dash/Scripts/DoorManager.cs
+++ Assets/Mad Dash/Scripts/DoorManager.cs
@@ -2,20 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class DoorGroupWrapper
+{
+    public List<Door> DoorGroup = new List<Door>();
+}
+
 public class DoorManager : MonoBehaviour
 {
     [SerializeField]
-    private List<List<Door>> DoorGroups = new List<List<Door>>();
+    public List<DoorGroupWrapper> doorGroups = new List<DoorGroupWrapper>();
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Tooltip("The time it takes for the open doors to switch.")] 
+    private float doorSwitchTime;
+
+    private bool switchDoorsComplete = true;
+
+    private void Update()
     {
-        
+        if (switchDoorsComplete)
+        {
+            StartCoroutine(SwitchDoors());
+            switchDoorsComplete = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SwitchDoors()
     {
-        
+        switchDoorsComplete = false;
+        foreach (DoorGroupWrapper doorGroupWrapper in doorGroups)
+        {
+            List<Door> doorGroup = doorGroupWrapper.DoorGroup;
+            int doorIndexToOpen = Random.Range(0, doorGroup.Count);
+            int currentDoorIndex = 0;
+            foreach (Door door in doorGroup)
+            {
+                door.open = false;
+                door.SetColor(Color.red);
+                if (currentDoorIndex == doorIndexToOpen)
+                {
+                    door.open = true;
+                    door.SetColor(Color.green);
+                }
+                currentDoorIndex += 1;
+            }
+        }
+        yield return new WaitForSeconds(doorSwitchTime);
+        switchDoorsComplete = true;
     }
 }
